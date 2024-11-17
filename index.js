7e775e87a4bb8f4507f59c4e725251b5b48f6620
BhargaviThanniru <bhargavithanniru432@gmail.com>
2024-11-17 15:02:05 +0530

    Initial commit: Add code to find even and odd numbers

---
+++ index.js
@@ -0,0 +1,15 @@
+// Function to find even numbers in an array
+function findEvenNumbers(arr) {
+    return arr.filter(num => num % 2 === 0);
+}
+
+// Function to find odd numbers in an array
+function findOddNumbers(arr) {
+    return arr.filter(num => num % 2 !== 0);
+}
+
+// Sample array of numbers
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log("Even Numbers:", findEvenNumbers(numbers));
+console.log("Odd Numbers:", findOddNumbers(numbers));
