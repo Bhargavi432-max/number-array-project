02564e3a85760e0fdb379134d1dffeb0f257b550
BhargaviThanniru <bhargavithanniru432@gmail.com>
2024-11-17 15:02:55 +0530

    Add pattern printing functionality

--- index.js
+++ index.js
@@ -13,3 +13,14 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 console.log("Even Numbers:", findEvenNumbers(numbers));
 console.log("Odd Numbers:", findOddNumbers(numbers));
+// Function to print a simple pattern
+function printPattern(n) {
+    let result = '';
+    for (let i = 1; i <= n; i++) {
+        result += '* '.repeat(i) + '\n';
+    }
+    console.log(result);
+}
+
+// Print a pattern of 5 rows
+printPattern(5);
